43085ed0d6b6f81c9a957bd2de82c69c1dd43fc4
byc <byc@example.com>
2024-02-29 07:22:36 -0600

    feat: coversion of money from EUR to USD

--- py/portfolio.py
+++ py/portfolio.py
@@ -5,13 +5,21 @@ from money import Money
 class Portfolio:
     def __init__(self):
         self.moneys = []
+        self._eur_to_used = 1.2
 
     def add(self, *moneys):
         self.moneys.extend(moneys)    
+    
+    def __convert(self, aMoney, aCurrency):
+        if aMoney.currency == aCurrency:
+            return aMoney.amount
+        else:
+            return aMoney.amount * self._eur_to_used
 
     def evaluate(self, currency):
         total = functools.reduce(
             # the 0 here (the last argument of reduce) is the initial value of the accumulator
-            operator.add, map(lambda m: m.amount, self.moneys), 0)
+            operator.add, map(lambda m: self.__convert(m, currency), self.moneys), 0
+            )
         return Money(total, currency)
 
